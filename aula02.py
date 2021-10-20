dac08345dcc709987d591b89d9aea07a1ec289d8
Diego Tasso <diegotblanco@gmail.com>
2021-10-20 11:34:32 -0300

    Primeiras aulas

---
+++ aula02.py
@@ -0,0 +1,12 @@
+x = 5
+print(x)
+y = 10 * 32 + x
+print(y)
+soma = x+y
+print(soma)
+frase = 'a massa ainda comerá do fino biscoito que fabrico'
+print(frase)
+a = 10
+b = 20
+soma = a + b
+print('A soma de {} + {} é {}'.format(a, b, soma))
